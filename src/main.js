d33b9834873f297ccecbcbe70a745ade879e879e
Sandon Lai <sandonnlai@gmail.com>
2022-08-29 12:12:04 +1000

    added fireflies shader

--- src/main.js
+++ src/main.js
@@ -3,10 +3,14 @@ import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import * as dat from "lil-gui";
 
+// Shaders
+import vertexShader from "./shaders/vertex.glsl";
+import fragmentShader from "./shaders/fragment.glsl";
+
 /**
  * Base
  */
-// Debug
+// Debug;
 // const gui = new dat.GUI({ width: 340 });
 
 // Canvas
@@ -15,34 +19,82 @@ const canvas = document.querySelector("canvas.webgl");
 // Scene
 const scene = new THREE.Scene();
 
+// Axes helper
+// const axesHelper = new THREE.AxesHelper(5);
+// scene.add(axesHelper);
+
 /**
  * Water
  */
 // Geometry
-const waterGeometry = new THREE.PlaneGeometry(2, 2, 128, 128);
+// const waterGeometry = new THREE.PlaneGeometry(2, 2, 128, 128);
+
+// // Material
+// const waterMaterial = new THREE.MeshBasicMaterial({ color: "#ff0000" });
+
+// // Mesh
+// const water = new THREE.Mesh(waterGeometry, waterMaterial);
+// water.rotation.x = -Math.PI * 0.5;
+// scene.add(water);
+
+/**
+ * Particles
+ */
+const firefliesGeometry = new THREE.BufferGeometry();
+const firefliesCount = 150;
+const positionArray = new Float32Array(firefliesCount * 3);
+const scaleArray = new Float32Array(firefliesCount * 1);
+
+for (let i = 0; i < firefliesCount; i++) {
+  positionArray[i * 3 + 0] = (Math.random() - 0.5) * 10;
+  positionArray[i * 3 + 1] = -(Math.random() * 10) * 10 + 50;
+  positionArray[i * 3 + 2] = (Math.random() - 0.5) * 10;
+
+  scaleArray[i] = Math.random();
+}
+
+firefliesGeometry.setAttribute(
+  "position",
+  new THREE.BufferAttribute(positionArray, 3)
+);
+firefliesGeometry.setAttribute(
+  "aScale",
+  new THREE.BufferAttribute(scaleArray, 1)
+);
 
 // Material
-const waterMaterial = new THREE.MeshBasicMaterial({ color: "#ff0000" });
+const firefliesMaterial = new THREE.ShaderMaterial({
+  uniforms: {
+    uTime: { value: 0 },
+    uPixelRatio: { value: Math.min(window.devicePixelRatio, 2) },
+    uSize: { value: 100 },
+  },
+  vertexShader: vertexShader,
+  fragmentShader: fragmentShader,
+  transparent: true,
+  depthWrite: false,
+});
 
-// Mesh
-const water = new THREE.Mesh(waterGeometry, waterMaterial);
-water.rotation.x = -Math.PI * 0.5;
-scene.add(water);
+const fireflies = new THREE.Points(firefliesGeometry, firefliesMaterial);
+scene.add(fireflies);
 
 /**
  * Sizes
  */
-const content = document.getElementsByClassName("content");
+const content = document.getElementById("content");
+console.log(content.offsetHeight);
 
 const sizes = {
   width: window.innerWidth,
-  height: content[0].offsetHeight,
+  height: content.offsetHeight,
+  // height: window.innerHeight,
 };
 
 window.addEventListener("resize", () => {
   // Update sizes
   sizes.width = window.innerWidth;
-  sizes.height = content[0].offsetHeight;
+  sizes.height = content.offsetHeight;
+  // sizes.height = window.innerHeight;
 
   // Update camera
   camera.aspect = sizes.width / sizes.height;
@@ -58,13 +110,15 @@ window.addEventListener("resize", () => {
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(
-  120,
+  150,
   sizes.width / sizes.height,
   0.1,
   100
 );
-camera.position.set(1, 1, 1);
+camera.position.set(1, 0, 1);
 scene.add(camera);
+
+// gui.add(camera.fov, "fov").min(120).max(200).step(5).name("cameraFOV");
 
 // Controls
 const controls = new OrbitControls(camera, canvas);
@@ -86,6 +140,8 @@ const clock = new THREE.Clock();
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
+
+  firefliesMaterial.uniforms.uTime.value = elapsedTime;
 
   // Update controls
   controls.update();
